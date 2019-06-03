adb2eb82633b37023dd5dd29219b318e22f39957
kouxiaohuai <49260903+kouxiaohuai@users.noreply.github.com>
2019-06-03 14:12:07 +0800

    Create GiniImpurity.py

---
+++ Iris/GiniImpurity.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 16:12:14 2019
+
+@author: kWX596514
+"""
+
+import math
+import numpy as np
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+iris = np.hstack((iris.data, iris.target[:, np.newaxis]))
+
+
+def split_iris(index, iris_sort):
+    iris_l = iris_sort[:index, :]
+    iris_r = iris_sort[index:, :]
+    return iris_l, iris_r
+
+
+def value_lr(iris_l, iris_r):
+    np_l_list = iris_l[:, -1].tolist()
+    np_r_list = iris_r[:, -1].tolist()
+    value_l = [np_l_list.count(0.0),
+               np_l_list.count(1.0),
+               np_l_list.count(2.0)]
+    value_r = [np_r_list.count(0.0),
+               np_r_list.count(1.0),
+               np_r_list.count(2.0)]
+    return value_l, value_r
+
+
+def gini_lr(value_l, value_r):
+    value_sum = sum(value_l) + sum(value_r)
+    p_l = [value_l[i]/sum(value_l) for i in range(len(value_l))]
+    p_r = [value_r[i]/sum(value_r) for i in range(len(value_r))]
+    e_l = 1 - sum([p_l[i]**2 for i in range(len(p_l))])
+    e_r = 1 - sum([p_r[i]**2 for i in range(len(p_r))])
+    return sum(value_l)/value_sum*e_l + sum(value_r)/value_sum*e_r
+
+
+for i in range(4):
+    arg = np.argsort(iris[:, i])
+    iris_sort = iris[arg]
+    
+    x = iris_sort[:, i]
+    y = []
+    
+    for j in range(1, len(x)):
+        iris_l, iris_r = split_iris(j, iris_sort)
+        value_l, value_r = value_lr(iris_l, iris_r)
+        ig = (1-(1/3)**2) - gini_lr(value_l, value_r)
+        y.append(ig)
+    
+    plt.grid(True)
+    plt.plot(x[1:], y)
+
+
+plt.legend(labels=[i for i in range(4)], loc=1, fontsize='24')
